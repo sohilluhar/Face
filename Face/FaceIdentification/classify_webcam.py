a286ccc2726116e0fc73ae4e60ca57644b4085fd
sohilluhar <sohilayubluhar@gmail.com>
2020-04-22 22:46:07 +0530

    added

--- Face/FaceIdentification/classify_webcam.py
+++ Face/FaceIdentification/classify_webcam.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import Normalizer
 from sklearn.svm import SVC
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
 
 
 def get_embedding(model, face_pixels):
@@ -29,102 +30,115 @@ def get_embedding(model, face_pixels):
 
 
 # Load the model
-keras_model = load_model('models/facenet_keras.h5')
+def runalgo():
+    keras_model = load_model('Face/FaceIdentification/models/facenet_keras.h5')
 
-# my comment
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-# load faces
-data = load('models/compressed_arrays/dataset.npz')
-testX_faces = data['arr_2']
+    # load faces
+    data = load('Face/FaceIdentification/models/compressed_arrays/dataset.npz')
+    testX_faces = data['arr_2']
 
-# load face embeddings
-data = load('models/compressed_arrays/face-embeddings.npz')
-trainX, trainY, testX, testY = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+    # load face embeddings
+    data = load('Face/FaceIdentification/models/compressed_arrays/face-embeddings.npz')
+    trainX, trainY, testX, testY = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 
-# normalize input vectors
-in_encoder = Normalizer(norm='l2')
-trainX = in_encoder.transform(trainX)
-testX = in_encoder.transform(testX)
+    # normalize input vectors
+    in_encoder = Normalizer(norm='l2')
+    trainX = in_encoder.transform(trainX)
+    testX = in_encoder.transform(testX)
 
-# label encode targets
-out_encoder = LabelEncoder()
-out_encoder.fit(trainY)
-trainY = out_encoder.transform(trainY)
-testY = out_encoder.transform(testY)
+    # label encode targets
+    out_encoder = LabelEncoder()
+    out_encoder.fit(trainY)
+    trainY = out_encoder.transform(trainY)
+    testY = out_encoder.transform(testY)
 
-# adding the labels
-for i in out_encoder.classes_:
-    if not os.path.exists('frames/' + i):
-        os.makedirs('frames/' + i)
+    # adding the labels
+    for i in out_encoder.classes_:
+        if not os.path.exists('Face/FaceIdentification/frames/' + i):
+            os.makedirs('Face/FaceIdentification/frames/' + i)
 
-# adding unknown
-if not os.path.exists('frames/unknowns'):
-    os.makedirs('frames/unknowns')
+    # adding unknown
+    if not os.path.exists('Face/FaceIdentification/frames/unknowns'):
+        os.makedirs('Face/FaceIdentification/frames/unknowns')
 
-# fit model
-model = SVC(kernel='linear', probability=True)
-model.fit(trainX, trainY)
+    # fit model
+    model = SVC(kernel='linear', probability=True)
+    model.fit(trainX, trainY)
 
-# creating video capture
-video = cv2.VideoCapture(0)
-while True:
-    ret_val, frame = video.read()
-    frame = cv2.flip(frame, 1)
-    # converting frame to numpy array of pixels
-    pixels = np.asarray(frame)
+    # creating video capture
+    video = cv2.VideoCapture(0)
+    myvar = 1
+    rvalue = ""
+    while True:
+        ret_val, frame = video.read()
+        frame = cv2.flip(frame, 1)
+        frameSize = (480, 480)
+        dim = (480, 480)
+        #	cam = VideoStream(src=0,  resolution=frameSize,framerate=32).start()
+        # img =cam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('camera', frame)
+        resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+        # converting frame to numpy array of pixels
+        pixels = np.asarray(frame)
 
-    # creating  detector using MTCNN
-    detector = MTCNN()
+        # creating  detector using MTCNN
+        detector = MTCNN()
+        if myvar == 3:
+            break
+        # getting results from face
+        results = detector.detect_faces(pixels)
 
-    # getting results from face
-    results = detector.detect_faces(pixels)
+        face_embeddings = list()
+        let_go = True
+        for i in results:
+            x1, y1, width, height = i['box']
+            x1, y1 = abs(x1), abs(y1)
+            x2, y2 = x1 + width, x1 + width
+            face = pixels[y1:y2, x1:x2]
+            # extract the face
+            try:
+                image = Image.fromarray(face)
+            except:
+                let_go = False
 
-    face_embeddings = list()
-    let_go = True
-    for i in results:
-        x1, y1, width, height = i['box']
-        x1, y1 = abs(x1), abs(y1)
-        x2, y2 = x1 + width, x1 + width
-        face = pixels[y1:y2, x1:x2]
-        # extract the face
-        try:
-            image = Image.fromarray(face)
-        except:
-            let_go = False
+            image = image.resize((160, 160))
+            face_array = np.asarray(image)
+            # create embeddings
+            face_embeddings.append(get_embedding(keras_model, face_array))
 
-        image = image.resize((160, 160))
-        face_array = np.asarray(image)
-        # create embeddings
-        face_embeddings.append(get_embedding(keras_model, face_array))
+        if let_go:
+            _ids = list()
+            for i in face_embeddings:
+                samples = expand_dims(i, axis=0)
+                yhat_class = model.predict(samples)
+                yhat_prob = model.predict_proba(samples)
+                yhat_class = model.predict(samples)
+                yhat_prob = model.predict_proba(samples)
 
-    if let_go:
-        _ids = list()
-        for i in face_embeddings:
-            samples = expand_dims(i, axis=0)
-            yhat_class = model.predict(samples)
-            yhat_prob = model.predict_proba(samples)
-            yhat_class = model.predict(samples)
-            yhat_prob = model.predict_proba(samples)
+                # get name and probabiltiy
+                class_index = yhat_class[0]
+                class_probability = yhat_prob[0, class_index] * 100
+                predict_names = out_encoder.inverse_transform(yhat_class)
+                print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+                rvalue = str(predict_names[0])
+                myvar = myvar + 1
+                if class_probability > 50:
+                    _ids.append(predict_names[0])
+                    cv2.imwrite('static/image.jpg', resized)
+                else:
+                    _ids.append("unknowns")
 
-            # get name and probabiltiy
-            class_index = yhat_class[0]
-            class_probability = yhat_prob[0, class_index] * 100
-            predict_names = out_encoder.inverse_transform(yhat_class)
-            print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
-            if class_probability > 50:
-                _ids.append(predict_names[0])
-            else:
-                _ids.append("unknowns")
+            current_timestamp = datetime.now()
+            for i in _ids:
+                cv2.imwrite('Face/FaceIdentification/frames/' + i + '/' + str(current_timestamp) + '.jpeg', frame)
 
-        current_timestamp = datetime.now()
-        for i in _ids:
-            cv2.imwrite('frames/' + i + '/' + str(current_timestamp) + '.jpeg', frame)
+            # cv2.imshow('window', frame)
+            if cv2.waitKey(1) == 27:
+                break  # esc to quit
 
-        # cv2.imshow('window', frame)
-        if cv2.waitKey(1) == 27:
-            break  # esc to quit
-
-cv2.destroyAllWindows()
-
+    cv2.destroyAllWindows()
+    # return rvalue
 # GET : /inhome/id
--- Face/view.py
+++ Face/view.py
@@ -2,14 +2,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
+# from Face.FaceIdentification.classify_webcam import runalgo
+
+
 def login(request):
     return render(request, 'login.html', {})
+
 
 def verify(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
     if email == "admin@gmail.com" and password == "admin":
-        return HttpResponseRedirect('/home')
+        return HttpResponseRedirect('home')
     else:
         return render(request, 'redirect.html',
                       {"swicon": "error", "swtitle": "Error", "swmsg": "Invalid Password or usrername",
@@ -19,9 +23,32 @@ def verify(request):
 def home(request):
     return render(request, 'home.html', {})
 
+
 def detect_criminal(request):
+    # rid = runalgo()
+    data = {}
+    user1 = {
+        "Name": "Yash Hassanandani",
+        "Age": "25",
+        "Gender": "Male",
+        "Number": "9819244040",
+        "Crime": "Chain snatching"
+    }
+    user2 = {
+        "Name": "Neha Phulwani",
+        "Age": "20",
+        "Gender": "Female",
+        "Number": "8169344463",
+        "Crime": "House theft"
+    }
+    rid = 1
+    if rid == 1:
+        data = user1
+    if rid == 2:
+        data = user2
+    print(data)
+    return render(request, 'test.html', data)
 
-    return render(request, 'home.html', {})
+
 def test(request):
-
     return render(request, 'test.html', {})
